f75ec5f30da8e9e95ecd089f3b5f61f5b72e664d
César Oliveira <cesar.oliveira@takelog.com.br>
2020-09-12 01:41:04 -0300

    Conclusão do projeto, falta melhorias possiveis

---
+++ js/script.js
@@ -0,0 +1,64 @@
+var audioPlayer = document.getElementById('audioplayer');
+var loaded = false
+
+var playBtn = document.getElementById('playBtn');
+var pauseBtn = document.getElementById('pauseBtn');
+
+document.getElementById('pauseBtn').addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    playBtn.style.display = "inline";
+    pauseBtn.style.display = "none";
+    audioPlayer.pause();
+
+
+    return false;
+});
+
+document.getElementById('playBtn').addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "inline";
+    audioPlayer.play();
+
+
+    return false;
+});
+
+
+const playSong = (file) => {
+    if(loaded ==false){
+        audioPlayer.innerHTML = `<source src="`+file+`" type="audio/mp3" />`;
+        loaded = true;
+    
+    }else {
+        audioPlayer.innerHTML = `<source src="${file}" type="audio/mp3" />`
+        loaded = false
+    }
+
+    audioPlayer.play();
+
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "inline";
+}
+
+document.querySelectorAll('.main_col').forEach(item =>{
+    item.addEventListener('click',event =>{
+        let image = item.getAttribute("data-image");
+        let artist = item.getAttribute("data-artist");
+        let song = item.getAttribute("data-song");
+        let file = item.getAttribute("data-file")
+
+        let playerArtistComponent = document.getElementsByClassName('player_artist');
+
+        playerArtistComponent[0].innerHTML = `
+        <img src="`+image+`" />
+        <h3>`+artist+`<br/><span>`+song+`</span></h3>
+        `;
+
+        playSong(file);
+
+    })
+
+});
